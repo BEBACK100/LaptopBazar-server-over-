79ed70b46cdcc51f9f5d74944019605283334d08
BEBACK100 <beback_bain@yahoo.com>
2022-11-25 17:10:48 +0600

    data api make

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+app.use(cors());
 
 const port = process.env.PORT || 5000;
 
 
-app.get('/')
+const homedisplaylap = require('./Homedata.json');
+const laptopdetails = require('./Alldata.json')
+
+app.get('/homeDisplay', (req, res) => {
+    res.send(homedisplaylap)
+})
+app.get('/allLaptop', (req, res) => {
+    res.send(laptopdetails)
+})
+
+app.get('/laptopdetails/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedlaptop = laptopdetails.find(n => n.id == id)
+    res.send(selectedlaptop)
+})
 
 app.get('/', (req, res) => {
     res.send('loptop bazer server is running')
