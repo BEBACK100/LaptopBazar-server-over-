3c343d9e54e2935d6f5d8c0aa31f2ae44811d726
BEBACK100 <beback_bain@yahoo.com>
2022-11-26 21:20:28 +0600

    mongodb error correction

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 const express = require('express');
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
 const app = express();
-app.use(cors());
+
+
+require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vczq7iu.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        const laptopCollection = client.db('oldLaptopbaze').collection('alldata')
+
+        app.get('/alldata', async (req, res) => {
+
+            const query = {};
+            const data = await laptopCollection.find(query).toArray();
+            res.send(data)
+        })
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
+
+
+
 
 const homedisplaylap = require('./Homedata.json');
-const laptopdetails = require('./Alldata.json')
+const laptopdetails = require('./Alldata.json');
+const { query } = require('express');
 
 app.get('/homeDisplay', (req, res) => {
     res.send(homedisplaylap)
